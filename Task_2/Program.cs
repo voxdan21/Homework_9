7abccaba43815713a04440b945b062b1e1f7d6e0
Voxdan21 <popov.daniil21@gmail.com>
2022-09-28 11:38:58 +0300

    Задание 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int SummGapNumber(int start, int end)
+{
+    if (end>=start)
+    {
+        end = end + SummGapNumber(start,end-1);
+    }
+    return end;
+}
+
+
+
+Console.WriteLine("Здраствуйте, введите пожайлуста значение N : ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Здраствуйте, введите пожайлуста значение M : ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+int result = SummGapNumber(number1,number2);
+Console.WriteLine($"Сумма чисел промежутка от {number1} до {number2}: {result}");
