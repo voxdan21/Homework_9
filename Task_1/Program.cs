e5fe64ae33ae9374de1bb222ab98102a80c20c13
Voxdan21 <popov.daniil21@gmail.com>
2022-09-28 11:38:53 +0300

    Задание 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string GapNumber(int x)
+{
+    string result = "";
+    if (x == 1)
+    {
+        result = "1";
+    }
+    else
+    {
+        result = GapNumber(x - 1) + "," + $"{x}";
+    }
+    return result;
+}
+
+
+
+
+
+
+Console.WriteLine("Здраствуйте, введите пожайлуста значение N : ");
+int number = Convert.ToInt32(Console.ReadLine());
+string result = GapNumber(number);
+Console.WriteLine($"Промежуток от 1 до {number}: {result}");
